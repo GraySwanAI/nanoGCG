18f81f2f0fe9f0a7d49fbb78eea902502d26bcb0
Justin Wang <justinwang46@gmail.com>
2024-07-09 23:16:50 +0000

    chore: get nonascii toks

--- nanogcg/utils.py
+++ nanogcg/utils.py
@@ -15,21 +15,21 @@ def get_nonascii_toks(tokenizer, device="cpu"):
     def is_ascii(s):
         return s.isascii() and s.isprintable()
 
-    ascii_toks = []
-    for i in range(3, tokenizer.vocab_size):
+    nonascii_toks = []
+    for i in range(tokenizer.vocab_size):
         if not is_ascii(tokenizer.decode([i])):
-            ascii_toks.append(i)
+            nonascii_toks.append(i)
     
     if tokenizer.bos_token_id is not None:
-        ascii_toks.append(tokenizer.bos_token_id)
+        nonascii_toks.append(tokenizer.bos_token_id)
     if tokenizer.eos_token_id is not None:
-        ascii_toks.append(tokenizer.eos_token_id)
+        nonascii_toks.append(tokenizer.eos_token_id)
     if tokenizer.pad_token_id is not None:
-        ascii_toks.append(tokenizer.pad_token_id)
+        nonascii_toks.append(tokenizer.pad_token_id)
     if tokenizer.unk_token_id is not None:
-        ascii_toks.append(tokenizer.unk_token_id)
+        nonascii_toks.append(tokenizer.unk_token_id)
     
-    return torch.tensor(ascii_toks, device=device)
+    return torch.tensor(nonascii_toks, device=device)
 
 def mellowmax(t: Tensor, alpha=1.0, dim=-1):
    return 1.0 / alpha * (torch.logsumexp(alpha * t, dim=dim) - torch.log(torch.tensor(t.shape[-1], dtype=t.dtype, device=t.device)))
